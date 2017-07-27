8c805b76f27e9e40e267e08efc2447cdf0c34d4a
Alex <alexzoetermeer@gmail.com>
2017-07-27 12:06:56 +0200

    [WIP] IT CAN READ
    validation to come

--- src/com/weaverplatform/absexcelconverter/util/WriteOperationParser.java
+++ src/com/weaverplatform/absexcelconverter/util/WriteOperationParser.java
@@ -1,45 +1,70 @@
 package com.weaverplatform.absexcelconverter.util;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
 
 import com.weaverplatform.absexcelconverter.util.model.Workbook;
 import com.weaverplatform.protocol.model.WriteOperation;
 
 public class WriteOperationParser {
-  
-  public static WriteOperation parse(Workbook workbook) {
-    Sheet datatypeSheet = (Sheet) workbook.getSheetAt(0);
-    Iterator<Row> iterator = datatypeSheet.iterator();
-  
-    while (iterator.hasNext()) {
-  
-        Row currentRow = iterator.next();
-        Iterator<Cell> cellIterator = currentRow.iterator();
-  
-        while (cellIterator.hasNext()) {
-  
-            Cell currentCell = cellIterator.next();
-            //getCellTypeEnum shown as deprecated for version 3.15
-            //getCellTypeEnum ill be renamed to getCellType starting from version 4.0
-            if (currentCell.getCellTypeEnum() == CellType.STRING) {
-                System.out.print(currentCell.getStringCellValue() + "--");
-            } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
-                System.out.print(currentCell.getNumericCellValue() + "--");
-            }
-  
-        }
-        System.out.println();
-  
-    }
-    
+
+  private volatile static List<String[]> data;
+
+  public synchronized static WriteOperation parse(Workbook workbook) {
+    String[][] data = read(workbook);
+    writeOut(data);
     return null;
+  }
+  //todo move this logic to Workbook
+  //todo create an interface to let a class
+  //determine is the supplied excel sheet is
+  //according to protocol and implement this logic in that class
+  //todo parse writeoperations in this class based on a valid
+  //excel sheet (the interface should have methods like
+  //get importId and getObjectnaam and such, according to columns
+  //enzo
+  private static String[][] read(Workbook workbook) {
+    data = new ArrayList<String[]>();
+    Sheet datatypeSheet = (Sheet) workbook.getSheetAt(0);
+    // This decides the length of each and every row following it
+    int lengthOfRowOne = datatypeSheet.getRow(0).getLastCellNum();
+    Iterator<Row> iterator = datatypeSheet.iterator();
+
+    while (iterator.hasNext()) {
+      Row currentRow = iterator.next();
+      List<String> cells = new ArrayList<String>();
+
+      for (int cn = 0; cn < lengthOfRowOne; cn++) {
+        Cell currentCell = currentRow.getCell(cn);
+        if (currentCell == null) {
+          cells.add("");
+        } else {
+          if (currentCell.getCellTypeEnum() == CellType.STRING) {
+            cells.add(currentCell.getStringCellValue());
+          } else if (currentCell.getCellTypeEnum() == CellType.NUMERIC) {
+            cells.add("" + currentCell.getNumericCellValue());
+          }
+        }
+      }
+      data.add(cells.toArray(new String[] {}));
+    }
+    return data.toArray(new String[][] {});
+  }
+  
+  //To be removed, debugging purposes only
+  private static void writeOut(String[][] data) {
+    for(String[] row : data) {
+      for(String str : row) {
+        System.out.print(str + "--");
+      }
+      System.out.println();
+    }
   }
 
 }
