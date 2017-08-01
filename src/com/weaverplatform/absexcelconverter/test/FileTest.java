3a1d2ca878cc4f812ead414f80412abab140d142
Alex <alexzoetermeer@gmail.com>
2017-08-01 14:41:17 +0200

    Finalized the REST test
    TODO: JUnit testing for the separate classes?

--- src/com/weaverplatform/absexcelconverter/test/FileTest.java
+++ src/com/weaverplatform/absexcelconverter/test/FileTest.java
@@ -1,7 +1,6 @@
 package com.weaverplatform.absexcelconverter.test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.equalTo;
 
 import java.io.File;
 import java.net.URISyntaxException;
@@ -14,12 +13,55 @@ public class FileTest extends BaseTest {
 
   @Test
   public void uploadTest() throws URISyntaxException {
-
+    
+    File file = new File(Resources.getResource("excel/abs_import_sheet(valid_1).xlsx").toURI());
+    
     given()
-      .multiPart(new File(Resources.getResource("abs_import_sheet(valid).xlsx").toURI()))
+      .multiPart(file)
       .expect()
       .statusCode(200)
-      .body(equalTo("OK"))
+      .when()
+      .post("http://localhost:4567/upload");
+
+  }
+  
+  @Test
+  public void upload2Test() throws URISyntaxException {
+    
+    File file = new File(Resources.getResource("excel/abs_import_sheet(valid_2).xlsx").toURI());
+    
+    given()
+      .multiPart(file)
+      .expect()
+      .statusCode(200)
+      .when()
+      .post("http://localhost:4567/upload");
+
+  }
+  
+  @Test
+  public void upload3Test() throws URISyntaxException {
+    
+    File file = new File(Resources.getResource("excel/abs_import_sheet(invalid_1).xlsx").toURI());
+    
+    given()
+      .multiPart(file)
+      .expect()
+      .statusCode(400)
+      .when()
+      .post("http://localhost:4567/upload");
+
+  }
+  
+  @Test
+  public void upload4Test() throws URISyntaxException {
+    
+    File file = new File(Resources.getResource("excel/abs_import_sheet(invalid_2).xlsx").toURI());
+    
+    given()
+      .multiPart(file)
+      .expect()
+      .statusCode(400)
       .when()
       .post("http://localhost:4567/upload");
 
